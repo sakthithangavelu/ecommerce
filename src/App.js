e8aa052abd2f0b353be1cc8355cbd7a5fc5b7cda
sakthithangavelu <sakthi.thangavelu@aspiresys.com>
2023-01-06 16:50:07 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import React from 'react'
-// import Home from './Pages/Home';
+import Home from './Pages/Home';
 import Navbar from './Components/Navbar';
-
+import Announcement from './Components/Announcement';
+import Slider from './Components/Slider';
 const App = () => {
   return (
     <div>
+          <Announcement/>
           <Navbar/>
+          <Slider/>
+          
     </div>
           
   )
---
+++ src/Components/Slider.js
@@ -0,0 +1,75 @@
+import { ArrowLeftOutlined, ArrowRightOutlined } from '@mui/icons-material';
+import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+        width:100%;
+        height: 100vh;
+        display:flex;
+        position: relative;
+
+`;
+const Arrow = styled.div`
+        width:50px;
+        height: 50px;
+        background-color:teal;
+        border-radius: 50%;
+        display: flex;
+        align-items:center;
+        justify-content:center;
+        position:absolute;
+        top:0;
+        bottom:0;
+        left: ${props => props.direction === "left" && "10px"};   //passing props in styled components for left arrow using conditional rendering short circuit operator
+        right: ${props => props.direction === "right" && "10px"}; //passing props in styled components for right arrow using conditional rendering short circuit operator
+        margin:auto;
+        cursor:pointer;
+        opacity:0.6;
+`;
+const Wrapper = styled.div`
+        height : 100%;
+`;
+const Slide = styled.div`
+        display: flex;
+        align-items:center;
+`;
+const ImgContainer = styled.div`
+        flex:1;
+`;
+const InfoContainer = styled.div`
+        flex:1;
+`;
+const Image = styled.img`
+`;
+
+
+
+
+
+const Slider = () => {
+    return (
+        <Container>
+            {/* passing props in styled components */}
+
+            <Arrow direction="left">
+                <ArrowLeftOutlined />
+            </Arrow>
+
+               <Wrapper>
+                    <ImgContainer>
+                         <Image src="https://www.ulcdn.net/media/furniture-stores/bangalore/yelahanka/Yelahanka_store_image.jpg?1658483963"/>
+                    </ImgContainer>
+                    <InfoContainer>
+
+                    </InfoContainer>
+
+               </Wrapper>
+
+            <Arrow direction="right">
+                <ArrowRightOutlined />
+            </Arrow>
+
+        </Container>
+    )
+}
+export default Slider
