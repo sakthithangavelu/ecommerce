427ffdeee27ba41875448d3d1386d1fee20fa2c7
sakthithangavelu <sakthi.thangavelu@aspiresys.com>
2023-01-18 11:38:28 +0530

    update

--- src/Pages/Register.js
+++ src/Pages/Register.js
@@ -1,8 +1,15 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styled from "styled-components";
 import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
 import ecommerceUrl from "../Axios/AxiosURL";
+import Icon from 'react-icons-kit';
+import { basic_eye } from 'react-icons-kit/linea/basic_eye';
+import { basic_eye_closed } from 'react-icons-kit/linea/basic_eye_closed';
+import { arrows_exclamation } from 'react-icons-kit/linea/arrows_exclamation'
+import { arrows_circle_check } from 'react-icons-kit/linea/arrows_circle_check'
+
+
 
 const Container = styled.div`
         width: 100vw;
@@ -17,26 +24,19 @@ const Wrapper = styled.div`
         padding: 20px;
         width: 20%; 
         background-color:white ;
+        
 `;
 const Title = styled.h1`
         font-size:24px;
         font-weight:3000;
 `;
 const Form = styled.form`
-        display:flex;
-        flex-wrap:wrap;
-        flex-direction:column;
-
 `;
 const Input = styled.input`
         flex: 1;
-        min-width:40%;
+        min-width:50%;
         margin:20px 10px 0px 0px;
         padding: 10px;
-`;
-const Agreement = styled.span`
-        font-size:12px;
-        margin:20px 0px;
 `;
 const Button = styled.button`
         width: 100%;
@@ -54,6 +54,51 @@ const Register = () => {
         const [lname, lnameChange] = useState("");
         const [email, emailChange] = useState("");
         const [password, passwordChange] = useState("");
+        const [type, setType] = useState("password");
+        // validated states
+        const [lowerValidated, setLowerValidated] = useState(false);
+        const [upperValidated, setUpperValidated] = useState(false);
+        const [numberValidated, setNumberValidated] = useState(false);
+        const [specialValidated, setSpecialValidated] = useState(false);
+        const [lengthValidated, setLengthValidated] = useState(false);
+        const lower = new RegExp('(?=.*[a-z])');
+        const upper = new RegExp('(?=.*[A-Z])');
+        const number = new RegExp('(?=.*[0-9])');
+        const special = new RegExp('(?=.*[!@#\$%\^&\*])');
+        const length = new RegExp('(?=.{8,})');
+
+        useEffect(() => {
+                if (lower.test(password)) {
+                        setLowerValidated(true);
+                }
+                else {
+                        setLowerValidated(false);
+                }
+                if (upper.test(password)) {
+                        setUpperValidated(true);
+                }
+                else {
+                        setUpperValidated(false);
+                }
+                if (number.test(password)) {
+                        setNumberValidated(true);
+                }
+                else {
+                        setNumberValidated(false);
+                }
+                if (special.test(password)) {
+                        setSpecialValidated(true);
+                }
+                else {
+                        setSpecialValidated(false);
+                }
+                if (length.test(password)) {
+                        setLengthValidated(true);
+                }
+                else {
+                        setLengthValidated(false);
+                }
+        }, [password]);
 
         const navigate = useNavigate();
 
@@ -77,10 +122,79 @@ const Register = () => {
                                         <Input value={fname} onChange={e => fnameChange(e.target.value)} type="text" placeholder="First name" required />
                                         <Input value={lname} onChange={e => lnameChange(e.target.value)} type="text" placeholder="Last name" required />
                                         <Input value={email} onChange={e => emailChange(e.target.value)} type="email" placeholder="Email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$" title="Please enter valid email [abc@mail.com]." required />
-                                        <Input value={password} onChange={e => passwordChange(e.target.value)} type="password" placeholder="Password" pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$" title="Please enter valid password of length of 8 or more [Must contain a special keyword, min of 1 character from a-z,A-Z and number]." required />
-                                        <Agreement>By Creating an account ,I consent to the processing of my personal
-                                                data in accordance with the <b>Privacy Policy</b>.
-                                        </Agreement>
+                                        <Input value={password}  type={type} onChange={e => passwordChange(e.target.value)} placeholder="Password" required />
+                                        {type === "password" ? (
+                                                <span className='icon-span' onClick={() => setType("text")}>
+                                                        <Icon icon={basic_eye_closed} size={18} />
+                                                </span>
+                                        ) : (
+                                                <span className='icon-span' onClick={() => setType("password")}>
+                                                        <Icon icon={basic_eye} size={18} />
+                                                </span>
+                                        )}
+                                        {/* validation tracker */}
+                                        <main className='tracker-box'>
+                                                <div className={lowerValidated ? 'validated' : 'not-validated'}>
+                                                        {lowerValidated ? (
+                                                                <span className='list-icon green'>
+                                                                        <Icon icon={arrows_circle_check} />
+                                                                </span>
+                                                        ) : (
+                                                                <span className='list-icon'>
+                                                                        <Icon icon={arrows_exclamation} />
+                                                                </span>
+                                                        )}
+                                                        At least one lowercase letter
+                                                </div>
+                                                <div className={upperValidated ? 'validated' : 'not-validated'}>
+                                                        {upperValidated ? (
+                                                                <span className='list-icon green'>
+                                                                        <Icon icon={arrows_circle_check} />
+                                                                </span>
+                                                        ) : (
+                                                                <span className='list-icon'>
+                                                                        <Icon icon={arrows_exclamation} />
+                                                                </span>
+                                                        )}
+                                                        At least one uppercase letter
+                                                </div>
+                                                <div className={numberValidated ? 'validated' : 'not-validated'}>
+                                                        {numberValidated ? (
+                                                                <span className='list-icon green'>
+                                                                        <Icon icon={arrows_circle_check} />
+                                                                </span>
+                                                        ) : (
+                                                                <span className='list-icon'>
+                                                                        <Icon icon={arrows_exclamation} />
+                                                                </span>
+                                                        )}
+                                                        At least one number
+                                                </div>
+                                                <div className={specialValidated ? 'validated' : 'not-validated'}>
+                                                        {specialValidated ? (
+                                                                <span className='list-icon green'>
+                                                                        <Icon icon={arrows_circle_check} />
+                                                                </span>
+                                                        ) : (
+                                                                <span className='list-icon'>
+                                                                        <Icon icon={arrows_exclamation} />
+                                                                </span>
+                                                        )}
+                                                        At least one special character
+                                                </div>
+                                                <div className={lengthValidated ? 'validated' : 'not-validated'}>
+                                                        {lengthValidated ? (
+                                                                <span className='list-icon green'>
+                                                                        <Icon icon={arrows_circle_check} />
+                                                                </span>
+                                                        ) : (
+                                                                <span className='list-icon'>
+                                                                        <Icon icon={arrows_exclamation} />
+                                                                </span>
+                                                        )}
+                                                        At least 8 characters
+                                                </div>
+                                        </main>
                                         <Button type="submit">CREATE</Button>
                                         <Link to="/"><Button>BACK</Button></Link>
                                 </Form>
--- src/data.js
+++ src/data.js
@@ -2,21 +2,21 @@
 export const sliderItems = [
   {
     id: 1,
-    img: "https://www.ulcdn.net/media/Slideshow/New-year-banner-Slideshow.jpg?1672511186",
+    img: "/assets/images/slider1.jpg",
     title: "New year and Pongal Sale...!",
     desc: "We specialize in the manufacture of household furniture,outdoor and office furniture.We use the very best materials available including teak woods.",
     bg: "c6f7ed",
   },
   {
     id: 2,
-    img: "https://www.ulcdn.net/images/products/149916/slide/666x363/0.jpg?1548906914",
+    img: "/assets/images/slider2.jpg",
     title: "Home Furnitures Available...!",
     desc: "We specialize in the manufacture of household furniture,outdoor and office furniture.We use the very best materials available including teak woods.",
     bg: "f7c6cf",
   },
   {
     id: 3,
-    img: "https://www.ulcdn.net/images/products/201809/slide/666x363/Truman_Study_Table_Creamy_Crust_Finish_Teak_merc.jpg?1624608693",
+    img: "/assets/images/slider3.jpg",
     title: "Office Furnitures Available...!",
     desc: "We specialize in the manufacture of household furniture,outdoor and office furniture.We use the very best materials available including teak woods.",
     bg: "faf3de",
