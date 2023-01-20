f5a040c0d48825933e9fd8ee28d8720f35758a0f
sakthithangavelu <sakthi.thangavelu@aspiresys.com>
2023-01-20 17:49:13 +0530

    update

--- src/Cart/ViewOrders.js
+++ src/Cart/ViewOrders.js
@@ -3,6 +3,44 @@ import Announcement from '../Components/Announcement'
 import Navbar from '../Components/Navbar'
 import Footer from '../Components/Footer'
 import ecommerceUrl from '../Axios/AxiosURL';
+import styled from 'styled-components';
+
+const Container = styled.div`
+`;
+const Wrapper = styled.div`
+        width: 100%;
+        background-color:#c6f7ed;
+        background:url("https://wallpaperaccess.com/full/2076146.jpg")center;
+        align-items: center;
+        justify-content:center;
+        margin-top: 20px;
+        height: 600px;
+        padding: 20px;
+        display: flex;
+        flex-direction: column;
+`;
+const DetailsContainer = styled.div`
+        display: flex;
+        flex-direction: row;
+        background: #f6f6f6;
+        border-radius: 0 0 5px 5px;
+        padding: 25px;
+`;
+const Image = styled.img`
+        max-height: 10rem;
+`;
+const InfoContainer = styled.div`
+`;
+const P = styled.p`
+        display: block;
+        color: #888;
+        margin: 0;
+        padding-left: 25px;
+`;
+const AddressContainer = styled.div`
+`;
+
+
 
 const ViewOrders = (product) => {
 
@@ -14,44 +52,44 @@ const ViewOrders = (product) => {
         ecommerceUrl.get("CartItems", { ...product, email }).then((res) => {
             ecommerceUrl.get("UserAddress").then((res) => {
                 setUserAddress(res.data);
-            
-              })
+
+            })
             setCartItems(res.data)
         })
-    },[]);
+    }, []);
 
-    const myCart=cartItems.filter(item=>{if(item.email===email) return item})
-    const myAddress=userAddress.filter(address=>{if(address.email===email) return address})
-    return(
-        <div>
-            <Announcement/>
-            <Navbar/>
-            <div className='viewform'>
-           
-        {
-             
-            myCart.map(item=>
-            <div className='cflex'>
-               
-                <img src={item.image} className='small'/>
-                <div >
-                <p>User Id :{item.email}</p>
-                <p>Product Price : Rs.{item.price}/-</p>
-                <p>Product name : {item.name}</p>
-                {(myAddress.map(address=>
-                <div>
-                <p>Address : {address.address1}{address.address2}</p>
-                <p>Postcode : {address.postcode}</p>
-                <p>Contact : {address.phone}</p>
-                </div>
-                ))}
-                </div>
-               </div>
-            )
-}
-        </div>
-           <Footer/>
-        </div>
+    const myCart = cartItems.filter(item => { if (item.email === email) return item })
+    const myAddress = userAddress.filter(address => { if (address.email === email) return address })
+    return (
+        <Container>
+            <Announcement />
+            <Navbar />
+            <Wrapper>
+
+                {
+
+                    myCart.map(item =>
+                        <DetailsContainer>
+
+                            <Image src={item.image} />
+                            <InfoContainer >
+                                <P>User Id :{item.email}</P>
+                                <P>Product Price : Rs.{item.price}/-</P>
+                                <P>Product name : {item.name}</P>
+                                {(myAddress.map(address =>
+                                    <AddressContainer>
+                                        <P>Address : {address.address1}{address.address2}</P>
+                                        <P>Postcode : {address.postcode}</P>
+                                        <P>Contact : {address.phone}</P>
+                                    </AddressContainer>
+                                ))}
+                            </InfoContainer>
+                        </DetailsContainer>
+                    )
+                }
+            </Wrapper>
+            <Footer />
+        </Container>
     )
 }
 export default ViewOrders
