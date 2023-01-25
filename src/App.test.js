300cdb142a96f423b9cb157d0018d2664a5d435c
sakthithangavelu <sakthi.thangavelu@aspiresys.com>
2023-01-25 17:43:13 +0530

    update

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,16 @@
-import { render, screen } from "@testing-library/react";
-import App from "./App";
+// import { render, screen } from "@testing-library/react";
+// import App from "./App";
 
-test("renders learn react link", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/BrOwN WoOdS/i);
-  expect(linkElement).toBeInTheDocument();
+// test("renders learn react link", () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/BrOwN WoOdS/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('render h1 element', () => {
+render(<App />);
+const linkElement = screen.getByText(/BrOwN WoOdS/i);
+expect(linkElement).toBeInTheDocument();
 });
